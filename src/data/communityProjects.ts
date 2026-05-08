963363f9f8ca25bcf6f5a6bddb0db50d50f6df83
enpicie <enpicie@gmail.com>
2026-05-08 16:35:28 -0500

    improve description

--- src/data/communityProjects.ts
+++ src/data/communityProjects.ts
@@ -12,7 +12,7 @@ export const communityProjects: CommunityProject[] = [
     slug: 'midweek-mashers',
     title: 'Midweek Mashers',
     summary:
-      'Founded an online competitive gaming community hub, building tooling to streamline the event experience and running multiple event series to support and grow the scene for a niche, old game. As the online community grew, attendance at major offline events increased from under 100 players to over 200 at large tournaments.',
+      'Founded an online competitive gaming community hub, building tooling to streamline the event experience and running multiple event series to support and grow the scene for a niche, old game. As the online community grew, attendance at major offline events increased from under 100 players to over 200 at large tournaments. Mentor both competitors and organizers, offering guidance and promoting personal and community growth for our scene while maintaining a fun, inclusive environment.',
     tags: ['Community Building', 'Event Organization', 'Discord', 'Streaming'],
     liveUrl: 'https://midweekmelting.com',
     iconFile: 'midweek-logo.png',
